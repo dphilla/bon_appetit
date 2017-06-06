5d2e4c8f85958fb1037344eea9b9f746e7476e54
Daniel Phillips <danieledward.phillips@gmail.com>
2017-06-06 09:19:00 -0600

    starts testing

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,22 @@
+
+
 class Pantry
+  attr_reader :stock
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    if @stock[item] == nil
+      return 0
+    else
+      @stock[item]
+    end 
+  end
+
+  def restock(item, amount)
+
+  end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,17 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  attr_reader :pantry
 
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, Pantry.new
+  end
+
+  def test_stock_is_empty
+    assert_equal pantry.stock, {}
+  end
 end
